705702e35ac36b6614de1909bc71ab81a9be3a69
jyqaz7548 <jyqaz7548@gmail.com>
2026-07-11 11:52:31 +0900

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { RootNavigator } from "@app/navigation";
 import { AppProviders } from "@app/providers";
 
 export default function App() {
   return (
-    <AppProviders>
-      <StatusBar style="dark" />
-      <RootNavigator />
-    </AppProviders>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppProviders>
+        <StatusBar style="dark" />
+        <RootNavigator />
+      </AppProviders>
+    </GestureHandlerRootView>
   );
 }
